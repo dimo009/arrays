f35ff5751d8bfe619eae03f93ad32649d88c058e
dimo009 <d_chavdarov@hotmail.com>
2016-10-06 07:41:21 +0300

    Add files via upload

---
+++ MostFrequentNumber8.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.HomeWork_Most_Frequent_Number
+{
+    class MostFrequentNumber8
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] distinctNumbers = numbers.Distinct().OrderBy(x=>x).ToArray();
+
+            int[] repeats = new int[distinctNumbers.Length];
+
+            for (int i = 0; i < distinctNumbers.Length; i++)
+            {
+                repeats[i] = numbers.Count(number => number == distinctNumbers[i]);
+            }
+
+            Console.WriteLine(distinctNumbers[repeats.ToList().IndexOf(repeats.Max())]);
+
+
+        }
+    }
+}
