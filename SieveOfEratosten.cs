352eaa915032b05ca993cba80ebcada0a84a5018
dimo009 <d_chavdarov@hotmail.com>
2016-10-06 16:31:37 +0300

    Add files via upload

---
+++ SieveOfEratosten.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.SieveOfEratosten
+{
+    class SieveOfEratosten
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            FindPrimes(n);
+        }
+
+        private static void FindPrimes(int n)
+        {
+            bool[] primes = new bool[n+1];
+
+            for (int i = 2; i <= n; i++)
+            {
+                primes[i] = true;
+            }
+            for (int i = 2; i <= n; i++)
+            {
+                if (!primes[i])
+                {
+                    continue;
+                }
+                Console.Write($"{i} ");
+                for (int j = i+i; j <= n; j+=i)
+                {
+                    primes[j] = false;
+                }
+            }
+        }
+    }
+}
