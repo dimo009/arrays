ce126612d7719c09896c99e69d222ce4d141a026
dimo009 <d_chavdarov@hotmail.com>
2016-10-06 07:46:44 +0300

    Add files via upload

---
+++ MostFrequentNumber8Second.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.HomeWork_Most_Frequent_Number
+{
+    class MostFrequentNumber8
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+
+            int resultNumber = 0;
+            int counter = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int tempCounter = 0;
+
+                foreach (var number in numbers)
+                {
+                    if (numbers[i]== number)
+                    {
+                        tempCounter++;
+                    }
+                }
+                if (tempCounter>counter)
+                {
+                    counter = tempCounter;
+                    resultNumber = numbers[i];
+
+                }
+            }
+            Console.WriteLine(resultNumber);
+
+        }
+    }
+}
