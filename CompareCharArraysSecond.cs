f502736f21037bda423c6a13d2fc0bc90020b7d4
dimo009 <d_chavdarov@hotmail.com>
2016-10-06 06:54:18 +0300

    Add files via upload

---
+++ CompareCharArraysSecond.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Homework_Compare_Char_Arrays_second
+{
+
+    class CompareCharArrays
+    {
+        static void Main(string[] args)
+        {
+            char[] firstArr = Console.ReadLine().Split().Select(char.Parse).ToArray();
+            char[] secondArr = Console.ReadLine().Split().Select(char.Parse).ToArray();
+
+            string[] arrays = { new string(firstArr), new string(secondArr) };
+            Console.WriteLine(string.Join("\n", arrays.OrderBy(str => str)));
+
+
+        }
+    }
+}
